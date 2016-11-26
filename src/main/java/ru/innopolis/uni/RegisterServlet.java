0583bc0ae54b5f9b7688139adcae569c30d08e32
MyName <you@example.com>
2016-11-26 13:50:28 +0300

    hash not equal

--- src/main/java/ru/innopolis/uni/RegisterServlet.java
+++ src/main/java/ru/innopolis/uni/RegisterServlet.java
@@ -7,9 +7,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
+import org.apache.commons.codec.binary.Hex;
 import java.util.List;
 import java.util.StringJoiner;
+
+import static org.apache.commons.codec.binary.Hex.encodeHex;
 
 /**
  * Created by olymp on 18.11.2016.
@@ -45,6 +50,18 @@ public class RegisterServlet extends HttpServlet {
         }
     }
 
+    private String calcHash(String data) {
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        messageDigest.update(data.getBytes());
+        String passHex = new String(Hex.encodeHex( messageDigest.digest() ));
+        return passHex;
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //at registration parameter "register" should be
@@ -61,16 +78,25 @@ public class RegisterServlet extends HttpServlet {
 
         try {
             Class.forName("org.h2.Driver");
-            try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test7");
+            try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test9");
                  Statement statement = connection.createStatement();) {
                 ForTest:
                 {
-                    statement.execute("CREATE TABLE IF NOT exists  Users(username VARCHAR(255) primary key,name VARCHAR(255), email VARCHAR(255),passhash BIGINT);");
+                    statement.execute("CREATE TABLE IF NOT exists  Users(username VARCHAR(255) primary key,name VARCHAR(255), email VARCHAR(255),passhash VARCHAR(255));");
                 }
 
                 if (register==null) {//this is login
                     ResultSet resultSet = statement.executeQuery("select passhash from Users where name=" + req.getAttribute("liUserName") + ";");
-                    if (resultSet.next() && req.getAttribute("liPassword") != null && resultSet.getLong(1) == Long.parseLong((String) req.getAttribute("liPassword"))) {
+
+                    if (resultSet.next()) {
+                        String db = resultSet.getString(1);
+                        String pass = liPassword, hpass = calcHash(liPassword);
+                        int i = 0;
+                        resultSet.first();
+                    }
+
+                    if (resultSet.next()  &&
+                            resultSet.getString(1).equals(calcHash(liPassword))) {
                         resp.addCookie(new Cookie("user", liUserName));
                         openSession(req,resp,liUserName);
                     } else {
@@ -78,7 +104,8 @@ public class RegisterServlet extends HttpServlet {
                         return;
                     }
                 } else { //this is registration
-                    statement.executeUpdate("INSERT INTO Users VALUES('"+liUserName+"','"+liName+"','"+liEmail+"',"+liPassword+");");
+                    String encryptedString = calcHash(liPassword);
+                    statement.executeUpdate("INSERT INTO Users VALUES('"+liUserName+"','"+liName+"','"+liEmail+"','"+encryptedString+"');");
                     openSession(req, resp, liUserName);
                 }
             } catch (SQLException e) {

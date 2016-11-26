64c9dd59aae3d415595738f431b213d21c126742
MyName <you@example.com>
2016-11-26 12:46:43 +0300

    Login/Registration session opens wout password hash

--- src/main/java/ru/innopolis/uni/RegisterServlet.java
+++ src/main/java/ru/innopolis/uni/RegisterServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.*;
+import java.util.List;
+import java.util.StringJoiner;
 
 /**
  * Created by olymp on 18.11.2016.
@@ -21,9 +23,41 @@ public class RegisterServlet extends HttpServlet {
 
     }
 
+    private boolean checkEmptyStrings(String[] params) {
+        for (String p : params)
+            if (p!=null && p.isEmpty())
+                return true;
+        return false;
+    }
+
+    private void warn(HttpServletRequest req, HttpServletResponse resp, String warning) throws ServletException, IOException {
+        req.setAttribute("warningMessage",warning);
+        getServletContext().getRequestDispatcher("/registration.jsp").forward(req,resp);
+    }
+
+    private void openSession(HttpServletRequest req, HttpServletResponse resp,String userName) {
+        OutputStream os = null;
+        try {
+            os = resp.getOutputStream();
+            os.write("<html><body>Hello, world!</body></html>".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        //at registration parameter "register" should be
+        String liName = (String)req.getParameter("liName");//at registration, else =null
+        String liUserName = (String)req.getParameter("liUserName");//at login
+        String liEmail = (String)req.getParameter("liEmail");//at registration
+        String liPassword = (String)req.getParameter("liPassword");//at login
+        String[] fields = {liName,liUserName,liEmail,liPassword};
+        if (checkEmptyStrings(fields)) {//if any field empty, return with warning
+            warn(req,resp,"Please, fill all fields");
+            return;
+        }
+        String register = (String)req.getParameter("register");
 
         try {
             Class.forName("org.h2.Driver");
@@ -31,20 +65,21 @@ public class RegisterServlet extends HttpServlet {
                  Statement statement = connection.createStatement();) {
                 ForTest:
                 {
-                    statement.execute("CREATE TABLE IF NOT Users(username VARCHAR(255) primary key,name VARCHAR(255)," +
-                            " email VARCHAR(255),passhash BIGINT);");
-                    statement.execute("insert into Product (username, name, email,passhash) " +
-                            "values ('sdf','sfff','email@email.ru',8457483958);");
+                    statement.execute("CREATE TABLE IF NOT exists  Users(username VARCHAR(255) primary key,name VARCHAR(255), email VARCHAR(255),passhash BIGINT);");
                 }
 
-                if (req.getParameter("liName")==null) {//this is login
-                    String liUserName = (String)req.getAttribute("liUserName");
-                    ResultSet resultSet = statement.executeQuery("select passhash from Users where name="+req.getAttribute("liUserName")+";");
-                    if (resultSet.next() && req.getAttribute("liPassword")!=null && resultSet.getLong(1)==Long.parseLong((String)req.getAttribute("liPassword"))) {
-                        resp.addCookie(new Cookie("user",liUserName));
+                if (register==null) {//this is login
+                    ResultSet resultSet = statement.executeQuery("select passhash from Users where name=" + req.getAttribute("liUserName") + ";");
+                    if (resultSet.next() && req.getAttribute("liPassword") != null && resultSet.getLong(1) == Long.parseLong((String) req.getAttribute("liPassword"))) {
+                        resp.addCookie(new Cookie("user", liUserName));
+                        openSession(req,resp,liUserName);
+                    } else {
+                        warn(req,resp,"No such user");
+                        return;
                     }
                 } else { //this is registration
-                    statement.executeQuery()
+                    statement.executeUpdate("INSERT INTO Users VALUES('"+liUserName+"','"+liName+"','"+liEmail+"',"+liPassword+");");
+                    openSession(req, resp, liUserName);
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -52,9 +87,5 @@ public class RegisterServlet extends HttpServlet {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
-
-        logger.info("post added !!!!"+req.getParameter("liName")+"  username:"+req.getParameter("liUserName")
-        +"   \nEmail:"+req.getParameter("liEmail")+"    Pass:"+req.getParameter("liPassword"));
     }
 }
